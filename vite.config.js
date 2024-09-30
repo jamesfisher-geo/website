46ca8579df05b9eed42750e046fad2ad0f08cd73
James <jafisher32@gmail.com>
2024-09-30 00:30:47 -0400

    isolation

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   base: '/website/',
   plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
     vue(),
     vueDevTools(),
   ],
