fd0f17d256edbcfc3c02109ad28966b4e61731bf
James <jafisher32@gmail.com>
2024-11-24 17:45:35 -0500

    sync

--- src/views/GaussianSplatView.vue
+++ src/views/GaussianSplatView.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="centered-window">
-    <GaussianSplatViewer
-      :mode="0"
-      :camera-up="[0, -1, -0.17]"
-      :initial-camera-position="[-5, -1, -1]"
-      :initial-camera-look-at="[-1.72477, 0.05395, -0.00147]"
-      :spherical-harmonics-degree="2"
-      asset-path=""
-    />
+    <GaussianSplatViewer :spherical-harmonics-degree="0" asset-path="" />
   </div>
 </template>
 
